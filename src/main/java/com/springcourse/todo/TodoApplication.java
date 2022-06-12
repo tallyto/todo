3b63d5ef6feb779aab91f02d3a50edf952f6c98f
tallyto <rodrigues.tallyto@hotmail.com>
2022-06-12 19:00:09 -0300

    model created

--- src/main/java/com/springcourse/todo/TodoApplication.java
+++ src/main/java/com/springcourse/todo/TodoApplication.java
@@ -2,16 +2,11 @@ package com.springcourse.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
-@RestController
 public class TodoApplication {
-	@GetMapping("/")
-	public String hello(){
-		return "Hello world";
-	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
---
+++ src/main/java/com/springcourse/todo/model/Todo.java
@@ -0,0 +1,29 @@
+package com.springcourse.todo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+// Data Class -> propriedades e getters e setters
+@Entity // entidade do banco de dados -> tabela do banco de dados
+@Getter // lombok -> gera os getters em tempo de compilação
+@Setter
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // definir coluna id como auto incremento 1, 2, 3 ...
+    private Long id; // chave primária
+
+    @Column
+    private String description;
+
+    @Column
+    private Boolean done;
+
+    @Column
+    private LocalDateTime createdDate;
+
+    @Column
+    private LocalDateTime doneDate;
+}
