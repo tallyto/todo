31147a269b66641d81403e77273f033f02bd4c3b
tallyto <rodrigues.tallyto@hotmail.com>
2022-06-12 19:55:09 -0300

    teste do banco de dados em memoria

--- src/main/java/com/springcourse/todo/TodoApplication.java
+++ src/main/java/com/springcourse/todo/TodoApplication.java
@@ -14,21 +14,21 @@ import java.time.LocalDateTime;
 @SpringBootApplication
 public class TodoApplication {
 
-
-	@Autowired // injetando o repositório
+	@Autowired // injetando dependência
 	private TodoRepository todoRepository;
-	@Bean
-	public CommandLineRunner init() { // método que será executado ao iniciar o spring boot
-	 return new CommandLineRunner() {
-		 @Override
-		 public void run(String... args) throws Exception {
-			 Todo todo = new Todo();
-			 todo.setDescription("Learn Spring Boot");
-			 todo.setCreatedDate(LocalDateTime.now());
-			 todoRepository.save(todo);
-		 }
-	 };
+	@Bean // testando a aplicação
+	public CommandLineRunner init() {
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... args) throws Exception {
+				Todo todo = new Todo();
+				todo.setDescription("Learn Spring Boot");
+				todo.setCreatedDate(LocalDateTime.now());
+				todoRepository.save(todo);
+			}
+		};
 	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
 	}
--- src/main/java/com/springcourse/todo/repository/TodoRepository.java
+++ src/main/java/com/springcourse/todo/repository/TodoRepository.java
@@ -2,9 +2,10 @@ package com.springcourse.todo.repository;
 
 import com.springcourse.todo.model.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
-// Model -> Todo
-// Id -> Long
+
 public interface TodoRepository extends JpaRepository<Todo,Long > {
+    // interface -> interface de acesso ao banco de dados
+
 
 
 }
