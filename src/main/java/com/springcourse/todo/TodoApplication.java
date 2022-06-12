219a0f2ddaa0a4043df1c402831da09ed6f6abb3
tallyto <rodrigues.tallyto@hotmail.com>
2022-06-12 20:24:31 -0300

    todo controller

--- src/main/java/com/springcourse/todo/TodoApplication.java
+++ src/main/java/com/springcourse/todo/TodoApplication.java
@@ -1,36 +1,16 @@
 package com.springcourse.todo;
 
-import com.springcourse.todo.model.Todo;
-import com.springcourse.todo.repository.TodoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import java.time.LocalDateTime;
 
 
 @SpringBootApplication
 public class TodoApplication {
 
-	@Autowired // injetando dependência
-	private TodoRepository todoRepository;
-	@Bean // testando a aplicação
-	public CommandLineRunner init() {
-		return new CommandLineRunner() {
-			@Override
-			public void run(String... args) throws Exception {
-				Todo todo = new Todo();
-				todo.setDescription("Learn Spring Boot");
-				todo.setCreatedDate(LocalDateTime.now());
-				todoRepository.save(todo);
-			}
-		};
-	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/springcourse/todo/rest/TodoController.java
@@ -0,0 +1,29 @@
+package com.springcourse.todo.rest;
+
+import com.springcourse.todo.model.Todo;
+import com.springcourse.todo.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController // indica que a classe é um controlador REST
+@RequestMapping("/api/todos") // indica o caminho da API
+public class TodoController {
+    @Autowired // injeta o repositório
+    private TodoRepository repository;
+
+    @PostMapping
+    public Todo save(@RequestBody Todo todo) { // recebe o todo via JSON
+        return repository.save(todo);
+    }
+
+    @GetMapping("{id}")
+    public Todo getById(Long id) {
+        return repository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                // se não encontrar o todo, lança exceção
+
+    }
+}
